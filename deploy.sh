abef16ef14ba82f29270f1e6d681b64c5ae92426
Nikolay Malevanny <nikolay.malevanny@axibase.com>
2015-02-18 13:38:46 +0000

    #792 Prepared to deploy to maven central.
    git-svn-id: https://localhost/svn/axibase/AxibaseAPI/atsd-api-java/trunk@8024 c89e5cec-f22d-a84c-b7a1-b0d050e0f212

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash
+
+ . deploy.config
+
+ mvn -s settings.xml -DargLine="-Daxibase.tsd.api.server.name=$ATSD_SERVER \
+ -Daxibase.tsd.api.server.port=$ATSD_PORT -Daxibase.tsd.api.username=$ATSD_USERNAME \
+ -Daxibase.tsd.api.password=$ATSD_PASSWORD" clean javadoc:jar \
+ source:jar gpg:sign -Dgpg.passphrase=$GPG_PASSPHRASE deploy
