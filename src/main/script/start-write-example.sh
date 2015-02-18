bf9ce036ffe57e818550fcdd793d041f1493dc7b
Nikolay Malevanny <nikolay.malevanny@axibase.com>
2015-02-18 15:34:24 +0000

    #800 Added scripts to start examples.
    git-svn-id: https://localhost/svn/axibase/AxibaseAPI/atsd-api-java/trunk@8040 c89e5cec-f22d-a84c-b7a1-b0d050e0f212

---
+++ src/main/script/start-write-example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+. client.config
+
+java -cp "lib/*" -Daxibase.tsd.api.server.name=$ATSD_SERVER \
+-Daxibase.tsd.api.server.port=$ATSD_PORT -Daxibase.tsd.api.username=$ATSD_USERNAME \
+-Daxibase.tsd.api.password=$ATSD_PASSWORD com.axibase.tsd.example.AtsdClientWriteExample
