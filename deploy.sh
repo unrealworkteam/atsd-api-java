d8b14498bb1bd2d916b116e4af78b0f4017a0e86
Nikolay Malevanny <zwave@ya.ru>
2016-02-02 18:48:25 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 . deploy.config
 
+export JAVA_HOME=$JAVA7_HOME
+
 mvn -s settings.xml -DargLine="-Daxibase.tsd.api.server.name=$ATSD_SERVER \
 -Daxibase.tsd.api.server.port=$ATSD_PORT -Daxibase.tsd.api.username=$ATSD_USERNAME \
 -Daxibase.tsd.api.password=$ATSD_PASSWORD" clean javadoc:jar \
